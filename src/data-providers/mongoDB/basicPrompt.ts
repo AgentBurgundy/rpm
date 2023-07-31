3b71b85ace61744b3c027883bb90e5e4649fa9e6
AgentBurgundy <professional.burgundy@gmail.com>
2023-07-31 04:18:30 -0500

    remove unnecessary

--- src/data-providers/mongoDB/basicPrompt.ts
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-
-const basicPromptSchema = new mongoose.Schema({
-  _id: String,
-  prompt: String,
-});
-
-const BasicPrompt = mongoose.model("Prompt", basicPromptSchema);
